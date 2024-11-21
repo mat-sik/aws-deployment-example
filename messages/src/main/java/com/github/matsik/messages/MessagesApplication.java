213f834edbe71b8fd16cb919689a7ba005bca7e5
Mateusz <msikorski@griddynamics.com>
2024-11-21 10:34:42 +0100

    Autoformat

--- messages/src/main/java/com/github/matsik/messages/MessagesApplication.java
+++ messages/src/main/java/com/github/matsik/messages/MessagesApplication.java
@@ -1,16 +1,10 @@
 package com.github.matsik.messages;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.lettuce.core.SetArgs;
-import io.lettuce.core.api.StatefulRedisConnection;
-import io.lettuce.core.api.sync.RedisCommands;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
-
-import java.time.Duration;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
