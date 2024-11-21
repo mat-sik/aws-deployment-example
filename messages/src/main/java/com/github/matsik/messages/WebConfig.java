d28c406c183f20da9fc0cb46c6a2f5d795cdb971
Mateusz <msikorski@griddynamics.com>
2024-11-21 10:34:10 +0100

    Configure cors for messages service.

---
+++ messages/src/main/java/com/github/matsik/messages/WebConfig.java
@@ -0,0 +1,28 @@
+package com.github.matsik.messages;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    private final String allowedOrigins;
+
+    public WebConfig(@Value("allowedOrigins") String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST")
+                .allowedHeaders("*")
+                .exposedHeaders("*")
+                .maxAge(3600);
+    }
+}
